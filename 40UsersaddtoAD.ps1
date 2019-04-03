3325af552536c24223ba7ca7dd4a319ef8d92d49
Sed3000 <Sedrick@10ronline.com>
2019-04-03 18:21:50 -0400

    Update 40UsersaddtoAD.ps1

--- 40UsersaddtoAD.ps1
+++ 40UsersaddtoAD.ps1
@@ -1,4 +1,6 @@
-$Users = Import-Csv -Path "c:\temp\40Users.csv"            
+#User list format: Firstname | Lastname | Maildomain | SAM | OU | Password | Description
+
+$Users = Import-Csv -Path "C:\users\swhite\documents\40users.csv"            
 foreach ($User in $Users)            
 {            
     $Displayname = $User.'Firstname' + " " + $User.'Lastname'            
@@ -6,8 +8,9 @@ foreach ($User in $Users)
     $UserLastname = $User.'Lastname'            
     $OU = $User.'OU'            
     $SAM = $User.'SAM'            
-    $UPN = $User.'Firstname' + "." + $User.'Lastname' + "@" + $User.'Maildomain'            
+    $UPN = $user.'SAM' + "@" + $User.'Maildomain'            
     $Description = $User.'Description'            
-    $Password = $User.'Password'            
-    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -server domain.loc            
+    $Password = $User.'Password' 
+    #echo $Displayname " "$SAM " " $UPN " " $Password " " $OU          
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName "$UserFirstname" -Surname "$UserLastname" -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $false –PasswordNeverExpires $true -path $OU      
 } 
